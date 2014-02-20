1504a877526e8e5fdd82e03f407547dbf7654e78
flapjacks <mattwbarry@gmail.com>
2014-02-20 13:03:48 -0500

    Update db_info.php

--- install/db_info.php
+++ install/db_info.php
@@ -1,7 +1,7 @@
 <?php
 
-$database = 'mwbarry_timeline';
-$username = 'mwbarry_timeUsr';
-$password = 't1M3leyeneUser';
+$database = '<db_name>';
+$username = '<db_user>';
+$password = '<db_pw>';
 
 ?>
